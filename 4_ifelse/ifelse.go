2f0b0a37d710d4d13448ed706b0646890a9a9336
timdevir07 <rajukumarsdr95@gmail.com>
2025-07-15 11:45:03 +0530

    Ex of if else

---
+++ 4_ifelse/ifelse.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	/*
+		age := 10
+		if age < 18 {
+			fmt.Println("U r kid")
+		} else if age > 18 && age >= 60 {
+			fmt.Println("u r adult")
+		} */
+}
+
+//output
+/*
+PS C:\Users\Raju Kumar\OneDrive\Desktop\Go Language\4_ifelse> go run ifelse.go
+U r kid
+*/
